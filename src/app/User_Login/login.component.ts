8abc0660ab172757a08e67dd9355a632514b38d2
Piyush <29piyushbora@gmail.com>
2023-02-03 14:18:06 +0530

    Login and Signup complete

--- src/app/User_Login/login.component.ts
+++ src/app/User_Login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 
-export class LoginComponent {
-  
+export class LoginComponent implements OnInit {
+
+  public loginForm !: FormGroup
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email : ['', Validators.required],
+      password : ['', Validators.required],
+    })
+  }
+
+  login() {
+    this.http.get<any>("http://192.168.0.144:5000/userLogin")
+    .subscribe(res=> {
+      const user = res.find ((a:any)=>{
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      });
+
+      if(user){
+        alert("Login Successfully");
+        this.loginForm.reset();
+      }
+      else{
+        alert("User Not Found! Pls try again!");
+        this.loginForm.reset();
+      }
+    },
+    err=>{
+      alert("Something Went Wrong!")
+    })
+  }
+
+  OnSubmit(){
+    if(this.loginForm.valid) {
+      this.login();
+    }
+    else{
+      console.log
+    }
+  }
 }
--- src/app/User_signup/signup.component.ts
+++ src/app/User_signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -11,7 +12,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class SignupComponent implements OnInit {
 
   public signupForm !: FormGroup;
-  constructor(private formBuilder: FormBuilder, private http : HttpClient) { }
+  constructor(private formBuilder: FormBuilder, private http : HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
@@ -23,9 +24,14 @@ export class SignupComponent implements OnInit {
   }
   
   signUp(){
-    this.http.post<any>("http://localhost:3000/signupUsers", this.signupForm.value)
-    .subscribe(res=>){
-
-    }
+    this.http.post<any>("http://192.168.0.144:5000/userRegistration", this.signupForm.value)
+    .subscribe(res=> {
+      alert("Signuop succesfull");
+      this.signupForm.reset();
+      this.router.navigate(['login']);
+    },
+    err=> {
+      alert("Sorry! Something went wrong!");
+    })
   }
 }
